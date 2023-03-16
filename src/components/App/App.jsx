05ac8628043f98768d26f100448c71a7edec8464
= <=>
2023-03-16 16:59:34 -0600

    Changed of BrowseRouter to HashRouter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from '../../routes/Home/HomePage';
 import { NewTaskPage } from '../../routes/NewTask/NewTaskPage';
 import { EditTaskPage } from '../../routes/EditTask/EditTaskPage';
@@ -7,14 +7,14 @@ import { EditTaskPage } from '../../routes/EditTask/EditTaskPage';
 
 function App() {
    return(
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path='/' element={<HomePage/>}/>
             <Route path='/new' element={<NewTaskPage/>}/>
             <Route path='/edit/:id' element={<EditTaskPage/>}/>
             <Route path='*' element={<p>Error 404</p>}/> 
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
     );
 }
 
